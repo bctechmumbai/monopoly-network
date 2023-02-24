ab4c80ea440c8fe77af2286a360b0d3732a3b48c
Lahu Waghmare <lahu.waghmare@nic.in>
2023-02-24 16:55:18 +0530

    removed unnecessary

--- WebApp/PIMS/EIS/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class EisConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'EIS'
--- WebApp/start-gunicorn-server.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# start-server.sh
-if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ; then
-    (cd PIMS; python manage.py createsuperuser --no-input)
-fi
-(cd PIMS; gunicorn PIMS.wsgi --user www-data --bind 0.0.0.0:8080 --workers 3) &
-nginx -g "daemon off;"
